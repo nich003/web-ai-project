259e5bf47bb86d37c75275f9dd84027def8c9464
nich003 <nima.cheraghi2003@gmail.com>
2025-06-18 12:57:55 +0200

    Added start of vue setup

---
+++ client/src/router/index.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import store from './store'
+import Home from '../views/Home.vue'
+
+const routes = [
+
+]
+
+const router = createRouter({
+    history:
+    routes:
+})
